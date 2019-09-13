0a16eb9485a5b8f8e0d22be53513a1dcf394e17f
Cory <coadams9@gmail.com>
2019-09-13 18:16:42 -0400

    working on login

--- espnCloneApp/app/javascript/components/Users.js
+++ espnCloneApp/app/javascript/components/Users.js
@@ -1,15 +1,47 @@
 import React from "react"
 import PropTypes from "prop-types"
+
+
 class Users extends React.Component {
-  render () {
+
+  state = {
+    email: '',
+    password: ''
+  }
+
+  handleSubmit = () => {
+    fetch('localhost:3000/login', {
+      method: 'POST',
+      headers: {
+        'Content-type': 'application/json'
+      },
+      body: JSON.stringify({
+        email: email,
+        password: password
+      })
+    })
+  }
+
+  handleChange = (e, { value }) => {
+    console.log(e)
+  }
+
+  render() {
+
+    const { email, password } = this.state
+
     return (
-     <div>
-        <h1>All Users</h1>
-        <ul>
-          {this.props.users.map(user => (
-            <li key={user.id}>{`${user.first_name} ${user.last_name} your email is ${user.email}`}</li>
-          ))}
-        </ul>
+      <div className='loginPage'>
+        <form className='loginContainer' onSubmit={this.handleSubmit}>
+          <h1>Login</h1>
+          <input type='text' placeholder='Email' onChange={(e) => this.handleChange} value={email.email} />
+          <br />
+          <input type='password' placeholder='Password' onChange={(e) => this.handleChange} value={password.password} />
+          <br />
+          <button type='submit'>Login</button>
+          <br />
+          <a href='register'>SignUp</a>
+        </form>
       </div>
     );
   }
--- espnCloneApp/app/models/user.rb
+++ espnCloneApp/app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_secure_password
+  validates :first_name, presence: true
+  validates :last_name, presence: true
+  validates :email, uniqueness: true
+  validates :password, :length => {:within => 6..18}
 end
