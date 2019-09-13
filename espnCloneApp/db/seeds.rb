27a8716e6deafb244784d8137dca54d8f2a03c90
Blair <blairacarroll@gmail.com>
2019-09-13 16:12:26 -0400

    rails && react setup working

--- espnCloneApp/db/seeds.rb
+++ espnCloneApp/db/seeds.rb
@@ -5,3 +5,6 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+cory = User.create(first_name: 'Cory', last_name: 'Adams', email: 'coadams9@gmail.com', password: 'cookies')
+blair = User.create(first_name: 'Blair', last_name: 'Carroll', email: 'blairacarroll@gmail.com', password: 'cookies')
